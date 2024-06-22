61047ef37100de8b96b6ea0cff2f40bcbd9294bc
RakeshV0498 <rockr4674@gmail.com>
2024-06-22 12:10:43 +0530

    API call for register

---
+++ src/apis/auth/register.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { backendUrl } from "../constants";
+
+export const userSignUp = async (userData) => {
+  try {
+    const response = await axios.post(`${backendUrl}register`, userData);
+    return await response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
