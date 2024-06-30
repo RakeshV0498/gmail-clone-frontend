0da1946cf5bbd71616765e2afdbfbbaf38363224
RakeshV0498 <rockr4674@gmail.com>
2024-06-30 09:51:16 +0530

    removed context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ContextProvider from "./context/ContextProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <App />
   </React.StrictMode>
 );
